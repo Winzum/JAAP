d61b18b1d30a3cabe14dba4dfbf8716385d74ef5
Winzum <winzum93@gmail.com>
2025-08-22 23:40:44 +0200

    add filedialog for export including filetype filters

--- main.gd
+++ main.gd
@@ -10,10 +10,13 @@ func _on_add_text_pressed() -> void:
 	graph_edit.add_graph_node()
 
 func _on_save_pressed() -> void:
-	show_file_dialog(FileDialog.FILE_MODE_SAVE_FILE, Callable(self, 'save_file'))
+	show_file_dialog(FileDialog.FILE_MODE_SAVE_FILE, 'json', Callable(self, 'save_file'))
 
 func _on_load_pressed() -> void:
-	show_file_dialog(FileDialog.FILE_MODE_OPEN_FILE, Callable(self, 'load_file'))
+	show_file_dialog(FileDialog.FILE_MODE_OPEN_FILE, 'json', Callable(self, 'load_file'))
+
+func _on_export_epub_pressed() -> void:
+	show_file_dialog(FileDialog.FILE_MODE_SAVE_FILE, 'epub', Callable(self, 'export_epub'))
 
 func _on_info_pressed() -> void:
 	var JaapInfo = JaapInfoScene.instantiate()
@@ -28,8 +31,73 @@ func _on_export_txt_pressed() -> void:
 		var texts_list = extract_node_connection_text(source_node, connections)
 		export_to_user_folder(texts_list)
 
-#formatting and ordering Epub pages with BFS
-func _on_export_epub_pressed() -> void:
+
+func _on_reset_pressed() -> void:
+	graph_edit.reset_graph_data()
+
+func get_origin_node(connections: Array) -> String:
+	var from_nodes = {}
+	var to_nodes = {}
+	var source_node = ""
+	
+	for connection in graph_edit.connections:
+		from_nodes[connection.from_node] = true
+		to_nodes[connection.to_node] = true
+		
+	for from_node in from_nodes:
+		if not to_nodes.has(from_node):
+			source_node = from_node
+			return source_node
+	#TODO what if no connections found?
+	return source_node
+
+#recurively extract the texts of all nodes
+func extract_node_connection_text(node_name: String, connections: Dictionary) -> Array:
+	var texts = []
+	var from_node = graph_edit.get_node(node_name)
+	var connected_ports = connections.get(node_name, {})
+	var from_node_TextEdits = from_node.find_children("TextEdit_*", "", false, false)
+	
+	for i in range(from_node_TextEdits.size()):
+		texts.append(from_node_TextEdits[i].text)
+		if connected_ports.has(i):
+			texts.append_array(extract_node_connection_text(connected_ports[i], connections))
+	return texts
+
+func export_to_user_folder(texts_list: Array) -> void:
+	var file_path = "user://exported_data.txt"
+	var file = FileAccess.open(file_path, FileAccess.WRITE)
+	if file:
+		for text in texts_list:
+			file.store_line(text)
+		file.close()
+	print("file exported to ", file_path)
+
+func show_file_dialog(mode: int, file_type: String, callable: Callable):
+	var dialog = FileDialog.new()
+	dialog.set_file_mode(mode)
+	dialog.set_access(FileDialog.ACCESS_FILESYSTEM)
+	dialog.set_use_native_dialog(true)
+	if file_type == "json":
+		dialog.add_filter("*.json", "JAAP save object notation")
+	if file_type == "epub":
+		dialog.add_filter("*.epub", "JAAP epub")
+	dialog.file_selected.connect(callable)
+	add_child(dialog)
+	dialog.popup_centered_ratio()
+
+func save_file(file_path: String) -> void:
+	var data = graph_edit.get_graph_data()
+	var file = FileAccess.open(file_path, FileAccess.WRITE)
+	file.store_string(JSON.stringify(data, "\t"))
+	file.close()
+
+func load_file(file_path: String) -> void:
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	var data = JSON.parse_string(file.get_as_text())
+	graph_edit.set_graph_data(data)
+
+func export_epub(file_path: String) -> void:
 	var graph_nodes = graph_edit.get_children().filter(func(node): return node is GraphNode)
 	var connections = graph_edit.get_formatted_connections(graph_edit.connections)
 	var origin_node = get_origin_node(graph_edit.connections)
@@ -93,65 +161,4 @@ func _on_export_epub_pressed() -> void:
 					section_content += "<hr>" + prev_text
 					epub_node.AddSection(section_title, section_content)
 
-		epub_node.ExportEpub("Sample.epub")
-
-func _on_reset_pressed() -> void:
-	graph_edit.reset_graph_data()
-
-func get_origin_node(connections: Array) -> String:
-	var from_nodes = {}
-	var to_nodes = {}
-	var source_node = ""
-	
-	for connection in graph_edit.connections:
-		from_nodes[connection.from_node] = true
-		to_nodes[connection.to_node] = true
-		
-	for from_node in from_nodes:
-		if not to_nodes.has(from_node):
-			source_node = from_node
-			return source_node
-	#TODO what if no connections found?
-	return source_node
-
-#recurively extract the texts of all nodes
-func extract_node_connection_text(node_name: String, connections: Dictionary) -> Array:
-	var texts = []
-	var from_node = graph_edit.get_node(node_name)
-	var connected_ports = connections.get(node_name, {})
-	var from_node_TextEdits = from_node.find_children("TextEdit_*", "", false, false)
-	
-	for i in range(from_node_TextEdits.size()):
-		texts.append(from_node_TextEdits[i].text)
-		if connected_ports.has(i):
-			texts.append_array(extract_node_connection_text(connected_ports[i], connections))
-	return texts
-
-func export_to_user_folder(texts_list: Array) -> void:
-	var file_path = "user://exported_data.txt"
-	var file = FileAccess.open(file_path, FileAccess.WRITE)
-	if file:
-		for text in texts_list:
-			file.store_line(text)
-		file.close()
-	print("file exported to ", file_path)
-
-func show_file_dialog(mode: int, callable: Callable):
-	var dialog = FileDialog.new()
-	dialog.set_file_mode(mode)
-	dialog.set_access(FileDialog.ACCESS_FILESYSTEM)
-	dialog.set_use_native_dialog(true)
-	dialog.file_selected.connect(callable)
-	add_child(dialog)
-	dialog.popup_centered_ratio()
-
-func save_file(file_path: String) -> void:
-	var data = graph_edit.get_graph_data()
-	var file = FileAccess.open(file_path, FileAccess.WRITE)
-	file.store_string(JSON.stringify(data, "\t"))
-	file.close()
-
-func load_file(file_path: String) -> void:
-	var file = FileAccess.open(file_path, FileAccess.READ)
-	var data = JSON.parse_string(file.get_as_text())
-	graph_edit.set_graph_data(data)
+		epub_node.ExportEpub(file_path)
