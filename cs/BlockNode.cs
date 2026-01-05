a4bdc75d5b3bce58d39921737cf4e52442c23515
Lennart van Winzum <winzum93@gmail.com>
2026-01-05 23:53:23 +0100

    deleting highlight block deletes row with same id

--- cs/BlockNode.cs
+++ cs/BlockNode.cs
@@ -2,35 +2,54 @@ using Godot;
 
 public abstract partial class BlockNode : GraphNode
 {
+	[Export] public Button CloseButton { get; set; }
+
+	private int BlockId { get; set; }
+
 	protected void Initialize(int canvas_id, int blocktype_id, float pos_x, float pos_y, string text = "")
 	{
-		var reader = DatabaseManager.ExecuteReader("SELECT name FROM sqlite_master WHERE type='table';");
-		foreach (var row in reader)  
-		{  
-			foreach (var kvp in row)  
-			{  
-				GD.Print($"{kvp.Key}: {kvp.Value}");  
-			}  
-		}
-		
 		var query = $"""
 		             insert into "block" ("canvas_id", "blocktype_id", "pos_x", "pos_y", "text")
 		             values ({canvas_id}, {blocktype_id}, {pos_x}, {pos_y}, '{text}');
 		             """;
-		GD.Print(query);
-		var queryresult = DatabaseManager.ExecuteNonQuery(query);
-		GD.Print("here2");
-		GD.Print(queryresult);
+		var result = DatabaseManager.ExecuteNonQuery(query);
+		if (result > 0)
+		{
+			var newId = DatabaseManager.ExecuteReader("""
+			                                          select id from block
+			                                          order by created_at desc
+			                                          limit 1;
+			                                          """)[0]["id"];
+			if (newId != null && int.TryParse(newId.ToString(), out int id))
+			{
+				BlockId = id;
+			}
+		}
 	}
-	
-	
+
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		if (CloseButton != null)
+		{
+			CloseButton.Pressed += _DeleteAndFree;
+		}
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	private void _DeleteAndFree()
 	{
+		var query =	 $"""
+		             delete from "block"
+		             where id = {BlockId};
+		             """;
+		GD.Print(query);
+		var result = DatabaseManager.ExecuteNonQuery(query);
+		if (result > 0)
+		{
+			QueueFree();
+		}
 	}
+
 }
+	
